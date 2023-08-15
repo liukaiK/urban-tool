bc36b06f6fe3ed6592eb98d261dfd5d9cceddc7d
liukai <zhizhufan@foxmail.com>
2023-08-15 16:50:28 +0800

    PhoneNumber

---
+++ urban-common/src/main/java/com/unicom/urban/common/entity/PhoneNumber.java
@@ -0,0 +1,53 @@
+package com.unicom.urban.common.entity;
+
+
+import com.unicom.urban.common.util.AESUtil;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Convert;
+import javax.persistence.Converter;
+import javax.persistence.Embeddable;
+
+/**
+ * @author liukai
+ */
+@Embeddable
+public class PhoneNumber {
+
+    @Convert(converter = PhoneNumberConverter.class)
+    private String phoneNumber;
+
+    protected PhoneNumber() {
+    }
+
+    public PhoneNumber(String phoneNumber) {
+        this.setPhoneNumber(phoneNumber);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * 手机号码加密
+     */
+    @Converter
+    public static class PhoneNumberConverter implements AttributeConverter<String, String> {
+
+        @Override
+        public String convertToDatabaseColumn(String attribute) {
+            return AESUtil.encrypt(attribute);
+        }
+
+        @Override
+        public String convertToEntityAttribute(String dbData) {
+            return AESUtil.decrypt(dbData);
+        }
+
+    }
+
+}
