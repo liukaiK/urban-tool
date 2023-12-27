8754c32d7eba64dac2bfd37b04f7026e29caf812
liukai <zhizhufan@foxmail.com>
2023-12-27 09:04:31 +0800

    update

--- urban-common/src/test/java/com/unicom/urban/common/util/LocalDateTimeUtilTest.java
+++ urban-common/src/test/java/com/unicom/urban/common/util/LocalDateTimeUtilTest.java
@@ -12,39 +12,41 @@ public class LocalDateTimeUtilTest {
 
     @Test
     public void thisQuarterStartOfDayTest() {
+        LocalDate today = LocalDate.now();
         int dayOfMonth = 1;
         int hour = 0;
         int minute = 0;
         int second = 0;
         LocalDateTime localDateTime = LocalDateTimeUtil.thisQuarterStartOfDay();
-        int monthValue = LocalDate.now().getMonthValue();
+        int monthValue = today.getMonthValue();
         if (monthValue <= 3) {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 1, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 1, dayOfMonth, hour, minute, second));
         } else if (monthValue <= 6) {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 4, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 4, dayOfMonth, hour, minute, second));
         } else if (monthValue <= 9) {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 7, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 7, dayOfMonth, hour, minute, second));
         } else {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 10, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 10, dayOfMonth, hour, minute, second));
         }
     }
 
     @Test
     public void thisQuarterEndOfDayTest() {
+        LocalDate today = LocalDate.now();
         int dayOfMonth = 31;
         int hour = 23;
         int minute = 59;
         int second = 59;
         LocalDateTime localDateTime = LocalDateTimeUtil.thisQuarterEndOfDay();
-        int monthValue = LocalDate.now().getMonthValue();
+        int monthValue = today.getMonthValue();
         if (monthValue <= 3) {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 3, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 3, dayOfMonth, hour, minute, second));
         } else if (monthValue <= 6) {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 6, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 6, dayOfMonth, hour, minute, second));
         } else if (monthValue <= 9) {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 9, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 9, dayOfMonth, hour, minute, second));
         } else {
-            Assert.assertEquals(localDateTime, LocalDateTime.of(LocalDate.now().getYear(), 12, dayOfMonth, hour, minute, second));
+            Assert.assertEquals(localDateTime, LocalDateTime.of(today.getYear(), 12, dayOfMonth, hour, minute, second));
         }
     }
 
