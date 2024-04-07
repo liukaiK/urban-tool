2ef72ff2282de6e44df8a2f788544fb6c47b2153
liukai <zhizhufan@foxmail.com>
2024-04-07 15:40:30 +0800

    增加工具类方法

--- urban-common/src/main/java/com/unicom/urban/common/util/HttpUtil.java
+++ urban-common/src/main/java/com/unicom/urban/common/util/HttpUtil.java
@@ -1,6 +1,7 @@
 package com.unicom.urban.common.util;
 
 import cn.hutool.extra.servlet.ServletUtil;
+import eu.bitwalker.useragentutils.UserAgent;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,6 +28,26 @@ public abstract class HttpUtil {
             return true;
         }
         return false;
+    }
+
+    /**
+     * 获取操作系统名称
+     */
+    public static String getOperatingSystem(HttpServletRequest request) {
+        UserAgent userAgent = getUserAgent(request);
+        return userAgent.getOperatingSystem().getName();
+    }
+
+    /**
+     * 获取浏览器
+     */
+    public static String getBrowser(HttpServletRequest request) {
+        UserAgent userAgent = getUserAgent(request);
+        return userAgent.getBrowser().getName();
+    }
+
+    private static UserAgent getUserAgent(HttpServletRequest request) {
+        return UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
     }
 
 
