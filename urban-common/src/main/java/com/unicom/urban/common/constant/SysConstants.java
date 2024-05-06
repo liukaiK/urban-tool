bdf5097ac75c9e41fb9cbe4df8de0324a96d8a60
liukai <zhizhufan@foxmail.com>
2024-05-06 09:40:04 +0800

    修改类型

--- urban-common/src/main/java/com/unicom/urban/common/constant/SysConstants.java
+++ urban-common/src/main/java/com/unicom/urban/common/constant/SysConstants.java
@@ -6,7 +6,7 @@ public class SysConstants {
 
     public static final String DELETED_FILED = "deleted";
 
-    public static final int DELETED_VALUE = 0;
+    public static final long DELETED_VALUE = 0;
 
     public static final String WHERE_DELETE = DELETED_FILED + "=" + DELETED_VALUE;
 
