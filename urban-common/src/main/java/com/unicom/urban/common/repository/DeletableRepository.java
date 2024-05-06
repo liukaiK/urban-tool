98d5be5923b4ca95dc61b6369e34dfa9fb2ab1b7
liukai <zhizhufan@foxmail.com>
2024-05-06 10:27:59 +0800

    修改使用的常量类

--- urban-common/src/main/java/com/unicom/urban/common/repository/DeletableRepository.java
+++ urban-common/src/main/java/com/unicom/urban/common/repository/DeletableRepository.java
@@ -1,6 +1,6 @@
 package com.unicom.urban.common.repository;
 
-import com.unicom.urban.common.constant.SysConstants;
+import com.unicom.urban.common.constant.DeletedConstants;
 import com.unicom.urban.common.entity.DeletableEntity;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,12 +15,12 @@ public interface DeletableRepository<T extends DeletableEntity, ID> {
 
     @Transactional
     @Modifying
-    @Query("update #{#entityName} set " + SysConstants.DELETED_FILED + " = id, deletedTime = :#{T(java.time.LocalDateTime).now()} where id = :id and " + SysConstants.WHERE_DELETE)
+    @Query("update #{#entityName} set " + DeletedConstants.DELETED_FILED + " = id, deletedTime = :#{T(java.time.LocalDateTime).now()} where id = :id and " + DeletedConstants.WHERE_DELETE)
     int softDeleteById(@Param("id") ID id);
 
     @Transactional
     @Modifying
-    @Query("update #{#entityName} set " + SysConstants.DELETED_FILED + " = id, deletedTime = :#{T(java.time.LocalDateTime).now()} where id in :ids and " + SysConstants.WHERE_DELETE)
+    @Query("update #{#entityName} set " + DeletedConstants.DELETED_FILED + " = id, deletedTime = :#{T(java.time.LocalDateTime).now()} where id in :ids and " + DeletedConstants.WHERE_DELETE)
     int softDeleteAllById(@Param("ids") Collection<? extends ID> ids);
 
 }
